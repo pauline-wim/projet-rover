5eaaff4b8886f2e45a6956df34727f06ba8c5f02
pauline-wim <linette.wim@hotmail.com>
2022-01-13 11:22:38 +0100

    added moveBackward function

--- projet.js
+++ projet.js
@@ -93,6 +93,54 @@ function moveForward(rover) {
     console.log(`y: ${rover.y}, x: ${rover.x}`);
 }
 
+// function moveBackward(rover) {
+//     rover.travelLog.push({y: rover.y, x: rover.x});
+//     // clear the trace from previous tour
+//     grid[rover.y][rover.x] = ' ';
+//     switch (rover.direction) {
+//         case "N":
+//             if (rover.y + 1 <= 9) {
+//                 rover.y += 1;   
+//             } else {
+//                 console.log("Error, you can't go this way.");
+//             }
+//             break;
+//         case "S":
+//             if (rover.y - 1 >= 0) {
+//                 rover.y -= 1;
+//             } else {
+//                 console.log("Error, you can't go this way.");
+//             }
+//             break;
+//         case "E":
+//             if (rover.x - 1 >= 0) {
+//                 rover.x -= 1;
+//             } else {
+//                 console.log("Error, you can't go this way.");
+//             }
+//             break;
+//         case "W":
+//             if (rover.x + 1 <= 9) {
+//                 rover.x += 1;
+//             } else {
+//                 console.log("Error, you can't go this way.");
+//             }
+//             break;
+//     }
+//     console.log(`y: ${rover.y}, x: ${rover.x}`);
+// }
+
+function turn180(rover) {
+    turnLeft(rover);
+    turnLeft(rover);
+}
+
+function moveBackward(rover) {
+    turn180(rover);
+    moveForward(rover);
+    turn180(rover);
+};
+
 function pilotRover(str) {
     str.split("").map((command) => { 
         switch (command) {
@@ -105,20 +153,27 @@ function pilotRover(str) {
             case "f":
                 moveForward(rover);
                 break;
+            case "b":
+                moveBackward(rover);
+                break;
         };
     })
 }
 
 function play() {
+    console.log("GET READY TO MOVE YOUR ROVER. (command q to quit play)")
     prompt.get({
         name: "command", 
         description: "Enter a command",
-        pattern: /^[lrf]+$/,
-        message: "Rover only understands following commands: 'l\[left\]', 'r\[ight\]', 'f\[orward\]'"},
+        pattern: /^[lrfqb]+$/,
+        message: "Rover only understands following commands: 'l\[left\]', 'r\[ight\]', 'f\[orward\]','b\[ackward\]', 'q\[uit\]'"},
         function(err, res) {
             if (err) {
                 console.log(err);
                 return 1;
+            }
+            if (res.command === "q") {
+                return;
             }
             pilotRover(res.command);
             grid[rover.y][rover.x] = rover.direction;
